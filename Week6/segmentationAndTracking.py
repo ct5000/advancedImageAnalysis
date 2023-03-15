80864c67d94e33890bf3f7a6d3c5a0e98300bdd8
ct5000 <crthul@gmail.com>
2023-03-15 13:39:44 +0100

    Updated week 6 to finish assignment and data uploaded for week 7

--- Week6/segmentationAndTracking.py
+++ Week6/segmentationAndTracking.py
@@ -11,8 +11,20 @@ def makeNormals(snake):
     N = snake.shape[1]
     normal = np.zeros([2,N])
     for i in range(N):
-        normal[:,i] = np.array([(snake[1,(i+1)%N]-snake[1,(i-1)%N])*(-1),(snake[0,(i+1)%N]-snake[0,(i-1)%N])*(-1)])
+        normal[:,i] = -np.array([(snake[1,(i+1)%N]-snake[1,(i-1)%N])*(-1),(snake[0,(i+1)%N]-snake[0,(i-1)%N])*(-1)])
     return normal
+
+def normalize(n):
+    return n/np.sqrt(np.sum(n**2,axis=0))
+
+
+def snake_normals(snake):
+    """ Returns snake normals. Expects snake to be 2-by-N array."""
+    ds = normalize(np.roll(snake, 1, axis=1) - snake) 
+    tangent = normalize(np.roll(ds,-1,axis=1) + ds)
+    normal = tangent[[1,0],:]*np.array([-1,1]).reshape([2,1])
+    return(normal)
+
 
 def curveSmoothImplicit(curve,alpha, beta):
     N = curve.shape[0]
@@ -30,30 +42,33 @@ def curveSmoothImplicit(curve,alpha, beta):
     return X
 
 def updateStep(Im,snake):
-    mask = skimage.draw.polygon2mask(Im.shape,snake.T).T
-
+    mask = skimage.draw.polygon2mask(Im.shape,snake.T)
     mean_inside = np.sum(np.sum(mask*Im)) / np.sum(np.sum(mask))
     mean_outside = np.sum(np.sum((1-mask)*Im)) / np.sum(np.sum(1-mask))
-    magnitude = 6* (mean_inside - mean_outside) * (2*Im[snake[0,:].astype(int),snake[1,:].astype(int)]-mean_inside-mean_outside)
-    print(magnitude[0])
-    normals = makeNormals(snake)
-    print(normals[:,0])
+    magnitude = 50*(mean_inside - mean_outside) * (2*Im.T[snake[0,:].astype(int),snake[1,:].astype(int)]-mean_inside-mean_outside)
+    #normals = makeNormals(snake)
+    normals = snake_normals(snake)
+
     newSnake = snake + magnitude*normals
-    newSnake = curveSmoothImplicit(newSnake.T,3,2).T
-    return newSnake
+    newSnake = curveSmoothImplicit(newSnake.T,0.1,0.1).T
+    return newSnake, normals
 
 
-vidoe = imageio.get_reader("crawling_amoeba.mov")
+video = imageio.get_reader("crawling_amoeba.mov")
 
-Im = skimage.io.imread("plusplus.png")
-Im = skimage.color.rgb2gray(Im)
+#Im = skimage.io.imread("plusplus.png")
+#Im = skimage.color.rgb2gray(Im) 
+
+Im = video.get_next_data()
+Im = skimage.color.rgb2gray(Im) 
 
 plt.figure()
 plt.imshow(Im)
 
-snake_size = 100
-snake_center = np.array([int(Im.shape[0]/2),int(Im.shape[1]/2)])
+snake_size = 125
+snake_center = np.array([int(Im.shape[1]/2),int(Im.shape[0]/2)])
 snake_radius = Im.shape[1]/6
+#snake_radius = 120
 print(snake_radius)
 print(snake_center)
 snake = np.zeros([2,snake_size])
@@ -63,22 +78,45 @@ for i in range(snake_size):
     snake[:,i] = (snake_center + snake_radius * np.array([np.cos(alpha),np.sin(alpha)]))
 
 plt.plot(snake[0,:],snake[1,:])
-plt.show()
+plt.show(block=False)
 
-for n in range(100):
-    snake = updateStep(Im,snake)
+for n in range(20):
+    print(n)
+    snake,normals = updateStep(Im,snake)
     snake = snake_functions.distribute_points(snake)
     snake = snake_functions.remove_intersections(snake)
-    plt.figure(n)
+    #plt.figure(n)
+    plt.clf()
     plt.imshow(Im)
     plt.plot(snake[0,:],snake[1,:])
+    '''
+    for i in range(snake_size):
+        plt.arrow(snake[0,i],snake[1,i],10*normals[0,i],10*normals[1,i],color='red')
+    '''
     plt.show(block=False)
-    plt.pause(0.3)
-    plt.close()
+    plt.pause(0.01)
+    #plt.close()
+
+for n in range(400):
+    Im = video.get_next_data()
+    Im = skimage.color.rgb2gray(Im) 
+    snake,normals = updateStep(Im,snake)
+    snake = snake_functions.distribute_points(snake)
+    snake = snake_functions.remove_intersections(snake)
+    #plt.figure(n)
+    plt.clf()
+    plt.imshow(Im)
+    plt.plot(snake[0,:],snake[1,:])
+    '''
+    for i in range(snake_size):
+        plt.arrow(snake[0,i],snake[1,i],10*normals[0,i],10*normals[1,i],color='red')
+    '''
+    plt.show(block=False)
+    plt.pause(0.01)
+    #plt.close()
 
 
-
-
+print("Finished")
 
 
 
@@ -86,4 +124,4 @@ for n in range(100):
 
 
 plt.show()
-vidoe.close()
+video.close()
